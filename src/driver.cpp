b2a6c36cdf85a539b54d89903d3ec95e8f2400e2
Shingirai98 <https://github.com/Shingirai98>
2021-04-13 04:37:07 +0200

    [ADDED] looping through operations

--- src/driver.cpp
+++ src/driver.cpp
@@ -5,129 +5,105 @@
 #include <sstream>
 #include <stdlib.h>
 #include <cstdlib>
-//#include "FrameSequence.h"
+#include "FrameSequence.h"
 //#include <bits/stdc++.h>
 
 // extractor declaration
-void extractor(std::string filename, int x1, int y1, int x2, int y2, int width, int height, std::string operation, std::string name);
+void extractor(std::string filename, int x1, int y1, int x2, int y2, int width, int height, std::vector<std::string> operation, std::vector<std::string> name);
 
 
 
 int main(int argc, char* argv[]) {
 
     int operations = 0;
-
+    
+    //if user doesnt put the other parameters
     if (argc==1){
-        std::cout << "Please specify the parameters ie....." << std::endl;
+        std::cerr << "Please specify the parameters ie....." << std::endl;
     }
+    //else assign the values to variables
     else{
-       
+       //fileName variable
         std::string fileName = argv[1];
+        //values for start and end points
         int x1 = atoi(argv[3]);
         int y1 = atoi(argv[4]);
         int x2 = atoi(argv[5]);
         int y2 = atoi(argv[6]);
         
+        //width and height of frames
         int width = atoi(argv[8]);
         int height = atoi(argv[9]);
         
-        std::string operation = argv[11];
-        std::string name = argv[12];
+        //vectors storing operations and names
+        std::vector<std::string> op;
+        std::vector<std::string> na;
         
-//        cout << fileName <<endl;
-//        cout << x1 <<endl;
-//        cout << y1 <<endl;
-//        cout << x2 <<endl;
-//        cout << y2 <<endl;
-//        cout << width <<endl;
-//        cout << height <<endl;
-//        cout << operation <<endl;
-//        cout << name << endl;
-        
-        //TODO: add a loop for multiple operations
-        
-        extractor(fileName, x1, y1, x2, y2, width, height, operation, name);
-        //FrameSequence FS;
+        //number storing the number of operations and names
+        int cont=0;
+        while(cont < argc-11){
+
+            op.push_back(argv[11+cont]);
+            na.push_back(argv[12+cont]);
+            cont = cont+3;
+        }
+        std::cout<<op[1]<<std::endl;
+        //call the extractor function
+        extractor(fileName, x1, y1, x2, y2, width, height, op, na);
+       
         
     }
 
     return 0;
 }
 
-void extractor(std::string filename, int x1, int y1, int x2, int y2, int width, int height, std::string operation, std::string name){
+void extractor(std::string filename, int x1, int y1, int x2, int y2, int width, int height, std::vector<std::string> operation, std::vector<std::string> name){
+    // read in the image file
     std::ifstream imageFile(filename, std::ios::binary);
+    
+    // condition if file is not read
     if(!imageFile){
         std::cerr << "Could not open file" << std::endl;
     }
+    
+    
     else{
-        std::string format;
-        std::string comment;
-        std::string c;
+        std::string format;// first line in PGM file
+        std::string comment;// comment in file
+        std::string c; // the dimensions
         std::string bins;
-        int rows;
-        int cols;
-        //std::vector<int> RnC; //Rows and columns
-        std::getline(imageFile , format);
-        std::getline(imageFile , comment);
-//        std::cout << format <<std::endl;
-//        std::cout << comment <<std::endl;
-        int i = 0;
-        std::getline(imageFile,c);
-        std::getline(imageFile, bins);
+        int rows; // y dim
+        int cols; // x dim
+        std::getline(imageFile , format); //ignore format
+        std::getline(imageFile , comment); // ignore comment TODO: loop through if there are multiple comments
+        std::getline(imageFile,c); // store dimensions in c
+        std::getline(imageFile, bins); // store range of BnW value
         std::istringstream ss(c);
         std::string X;
         std::string Y;
-        ss >> X;
+         // assign x dimension
+        ss >> X; // assign x dimension
         ss >> Y;
+        // convert to integers
+        cols = stoi(X);
+        rows = stoi(Y);
         
-        rows = stoi(X);
-        cols = stoi(Y);
-        
-        //std::cout << rows << " & " << cols << ". The number of characters are: " << rows*cols<<std::endl;
+        // read the file and point to it using the buffer pointer
         char * buffer = new char [rows*cols];
-        imageFile.read(buffer,( (rows*cols)));
-//        FrameSequence frame;
-//        frame.setSize(width, height);
-//        frame.setStartnEnd(x1, y1, x2, y2);
-//
-//        frame.setFrames(buffer);
-        std::cout<<buffer<<std::endl;
-//        while ( std::getline (imageFile,bins) ){
-//              //std::cout << bins << std::endl;
-//        }
+    
+        imageFile.read(buffer, (rows*cols));
+        FrameSequence frame;
         imageFile.close();
-        delete[] buffer;
+        frame.setFrameSize(width, height);
+        frame.setStartnEnd(x1, y1, x2, y2);
+
+        frame.setFrames(buffer, operation[0], name[0]);
+       
+        
+        //outputFile.write(bo, (width*height));
+        //std::cout<<(int)(*buffer)<<std::endl;
+        
+        //delete[] buffer;
     
     }
-}//,std::string operation,std::string name){
-//    ifstream imageFile(filename, ios::binary);
-//    if(!imageFile){
-//        cerr << "Could not open file" ;
-//    }
-//    else{
-//        string format;
-//        string comment;
-//        getline(imageFile , format);
-//
-//        getline(imageFile,  comment);
-//
-//        int cols, rows, intensity;
-//        imageFile >> cols >> std::ws;
-//        imageFile >> rows >> std::ws;
-//        imageFile >> intensity>> std::ws;
-//        const int ARRAY_SZ = rows*cols;
-//        char *array = new char [ARRAY_SZ];
-//        char arrayPGM[cols][rows];
-//
-//        /*while(!imageFile.eof()){
-//            imageFile.read(array, ARRAY_SZ);
-//            for(int i=0; i<ARRAY_SZ; ++i){
-//                std::cout <<array[i];
-//            }
-//            cout << endl;
-//        }*/
-//
-//    }
-//    //outfile.write(array, ARRAY_SZ);
-//}
-//
+}
